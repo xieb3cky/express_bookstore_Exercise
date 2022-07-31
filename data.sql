03bc04bf9e90cc67038247292b26c1411d49b2a7
becky <xieb3cky@gmail.com>
2022-07-31 18:50:05 -0400

    express bookstore

---
+++ data.sql
@@ -0,0 +1,15 @@
+\c books
+
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE books (
+  isbn TEXT PRIMARY KEY,
+  amazon_url TEXT,
+  author TEXT,
+  language TEXT, 
+  pages INTEGER,
+  publisher TEXT,
+  title TEXT, 
+  year INTEGER
+);
